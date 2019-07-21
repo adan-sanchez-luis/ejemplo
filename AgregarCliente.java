860268710eca84386dda2bffd9deb86dbc5c424b
adan sanchez <sanchezadan142@gmail.com>
2019-07-21 13:58:03 -0500

    se agrego las funcionalidades de cliente

--- AgregarCliente.java
+++ AgregarCliente.java
@@ -1,4 +1,3 @@
-
 import java.awt.*;
 
 import javax.swing.*;
@@ -13,208 +12,209 @@ import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.border.Border;
+public class AgregarCliente extends JFrame{
+	
+	
+	AgregarCliente(){
+		
+		setSize(1100,600);
+		setTitle("Agregar clientes");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+		setResizable(false);
+		
+		JPanel Clientes= new JPanel();
+		Clientes.setLayout(null);
+		Clientes.setSize(1100,600);
+		Clientes.setBackground(Color.black);
 
-public class AgregarCliente extends JFrame {
+		JLabel anuncio = new JLabel("Agregar datos de los clientes:");
+		anuncio.setForeground(Color.white);
+		Font fuente=new Font("Arial Black", Font.BOLD, 20);
+		anuncio.setFont(fuente);
+		anuncio.setBounds(370,0,400,70);
+		Clientes.add(anuncio);
+		
+		JLabel nombresResponsable= new JLabel("Nombre(s):");
+		nombresResponsable.setForeground(Color.white);
+		Font fuenteResponsables=new Font("Arial",Font.BOLD,20);
+		nombresResponsable.setFont(fuenteResponsables);
+		nombresResponsable.setBounds(0,30,280,230);
+		Clientes.add(nombresResponsable);
+		
+		JTextField NombreResponsabletxt = new JTextField("");
+		NombreResponsabletxt.setForeground(Color.black);
+		NombreResponsabletxt.setBounds(128,135,200,30);
+		NombreResponsabletxt.setBorder(null);
+		Clientes.add(NombreResponsabletxt);
+		
+		JLabel ApellidoResponsablePaterno= new JLabel("Apellido paterno:");
+		ApellidoResponsablePaterno.setForeground(Color.white);
+		Font fuenteResponsablesP=new Font("Arial",Font.BOLD,20);
+		ApellidoResponsablePaterno.setFont(fuenteResponsablesP);
+		ApellidoResponsablePaterno.setBounds(340,30,350,230);
+		Clientes.add(ApellidoResponsablePaterno);
+	
+		JTextField ApellidoResponsablePaternotxt = new JTextField("");
+		ApellidoResponsablePaternotxt.setForeground(Color.black);
+		ApellidoResponsablePaternotxt.setBounds(502,135,200,30);
+		ApellidoResponsablePaternotxt.setBorder(null);
+		Clientes.add(ApellidoResponsablePaternotxt);
+		
+		JLabel ApellidoResponsableMaterno= new JLabel("Apellido materno:");
+		ApellidoResponsableMaterno.setForeground(Color.white);
+		Font fuenteResponsablesMaterno=new Font("Arial",Font.BOLD,20);
+		ApellidoResponsableMaterno.setFont(fuenteResponsables);
+		ApellidoResponsableMaterno.setBounds(720,30,200,230);
+		Clientes.add(ApellidoResponsableMaterno);
+		
+		
+		JTextField ApellidoResponsableMaternotxt = new JTextField("");
+		ApellidoResponsableMaternotxt.setForeground(Color.black);
+		ApellidoResponsableMaternotxt.setBounds(890,135,200,30);
+		ApellidoResponsableMaternotxt.setBorder(null);
+		Clientes.add(ApellidoResponsableMaternotxt);
+		
+		JLabel Telefono = new JLabel("Télefono:");
+		Telefono.setForeground(Color.white);
+		Font fuenteTelefono=new Font("Arial",Font.BOLD,20);
+		Telefono.setFont(fuenteTelefono);
+		Telefono.setBounds(0,70,200,230);
+		Clientes.add(Telefono);
+		
+		JTextField Telefonotxt = new JTextField("");
+		Telefonotxt.setForeground(Color.black);
+		Telefonotxt.setBounds(128,176,200,30);
+		Telefonotxt.setBorder(null);
+		Clientes.add(Telefonotxt);
+		
+		
+		JLabel Correo = new JLabel("E-mail:");
+		Correo.setForeground(Color.white);
+		Font fuenteCorreo=new Font("Arial",Font.BOLD,20);
+		Correo.setFont(fuenteCorreo);
+		Correo.setBounds(435,113,260, 150);
+		Clientes.add(Correo);
+		
+		JTextField Correotxt = new JTextField("");
+		Correotxt.setForeground(Color.black);
+		Correotxt.setBounds(502,178,200,30);
+		Correotxt.setBorder(null);
+		Clientes.add(Correotxt);
+		
+		JLabel empresa = new JLabel("Empresa:");
+		empresa.setForeground(Color.white);
+		Font fuenteEmpresa= new Font("Arial",Font.BOLD,20);
+		empresa.setFont(fuenteEmpresa);
+		empresa.setBounds(797,112,300,150);
+		Clientes.add(empresa);
+		
+		JTextField empresatxt= new JTextField("");
+		empresatxt.setForeground(Color.black);
+		empresatxt.setBorder(null);
+		empresatxt.setBounds(890,178,200,30);
+		Clientes.add(empresatxt);
+		
+		
+		
+		
+		JLabel Domicilio= new JLabel("Datos domiciliarios:");
+		Domicilio.setForeground(Color.white);
+		Font fuenteDomicilio= new Font("Arial Black",Font.BOLD,20);
+	    Domicilio.setFont(fuenteDomicilio);
+		Domicilio.setBounds(410,100,300,300);
+		Clientes.add(Domicilio);
+		
+		JLabel Calle = new JLabel("Calle:");
+		Calle.setForeground(Color.white);
+		Font fontCalle=new Font("Arial",Font.BOLD,20);
+		Calle.setFont(fontCalle);
+		Calle.setBounds(0,166,300,300);
+		Clientes.add(Calle);
+		
+		JTextField Calletxt = new JTextField("");
+		Calletxt.setForeground(Color.black);
+		Calletxt.setBounds(130,300,200,30);
+		Calletxt.setBorder(null);
+		Clientes.add(Calletxt);
+		
+		JLabel Numero= new JLabel("Número:");
+		Numero.setForeground(Color.white);
+		Font fontNumero=new Font("Arial",Font.BOLD,20);
+		Numero.setFont(fontNumero);
+		Numero.setBounds(418,166,300,300);
+		Clientes.add(Numero);
+		
+		JTextField Numtxt = new JTextField("");
+		Numtxt.setForeground(Color.black);
+		Numtxt.setBounds(503,300,40,30);
+		Numtxt.setBorder(null);
+		Clientes.add(Numtxt);
+		
+		JLabel colonia = new JLabel("Colonia:");
+	    colonia.setForeground(Color.white);
+	    Font fontColonia = new Font("Arial",Font.BOLD,20);
+	    colonia.setFont(fontColonia);
+	    colonia.setBounds(805,166,300,300);
+		Clientes.add(colonia);
+		
+		JTextField Coltxt= new JTextField();
+		Coltxt.setForeground(Color.black);
+		Coltxt.setBounds(890,300,200,30);
+		Coltxt.setBorder(null);
+	    Clientes.add(Coltxt);
+	    
+	
+		
+		JLabel Municipio = new JLabel("Municipio:");
+		Municipio.setForeground(Color.white);
+		Font fontMunicipio=new Font("Arial",Font.BOLD,20);
+		Municipio.setFont(fontMunicipio);
+		Municipio.setBounds(0,210,300,300);
+		Clientes.add(Municipio);
+		
+		//JComboBox Municipiotxt = new JComboBox();
+		JTextField Municipiotxt= new JTextField();
+		Municipiotxt.setForeground(Color.black);
+		Municipiotxt.setBounds(128,350,200,30);
+		Municipiotxt.setBorder(null);
+		Clientes.add(Municipiotxt);
+		
+		JLabel Estado= new JLabel("Estado:");
+		Estado.setForeground(Color.white);
+		Font fontEstado= new Font("Arial",Font.BOLD,20);
+		Estado.setFont(fontEstado);
+		Estado.setBounds(427,210,100,300);
+		Clientes.add(Estado);
+		
+		JTextField Estadotxt= new JTextField();
+		Estadotxt.setForeground(Color.black);
+		Estadotxt.setBounds(503,350,170,30);
+		Estadotxt.setBorder(null);
+		Clientes.add(Estadotxt);
 
-    AgregarCliente() {
+	     JButton Agregar = new JButton("Agregar cliente");
+	      Agregar.setBackground(Color.black);
+	  	Font fontBoton= new Font("Arial",Font.BOLD,20);
+	  	Agregar.setFont(fontBoton);
+	        //entrar.setBounds(110, 350, 150, 50);
+	        Agregar.setBorder(new ComponenteBotonRedondo(40));
+	        Agregar.setForeground(Color.decode("#049cff"));
+	        Agregar.setBounds(410, 430, 250, 50);
+	        Clientes.add(Agregar);
 
-        setSize(683, 500);
-        setTitle("Agregar clientes");
-        //setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setVisible(true);
-        setResizable(false);
+		JLabel background = new JLabel();
+		background.add(Clientes);
+		add(background );
+		setVisible(true);
 
-        JPanel Clientes = new JPanel();
-        Clientes.setLayout(null);
-        Clientes.setSize(1600, 768);
-        Clientes.setBackground(Color.decode("#049cff"));
-
-        JLabel anuncio = new JLabel("Agregar datos de los clientes:");
-        anuncio.setForeground(Color.white);
-        Font fuente = new Font("Arial", Font.BOLD, 16);
-        anuncio.setFont(fuente);
-        anuncio.setBounds(250, 0, 300, 70);
-        Clientes.add(anuncio);
-
-        JLabel empresa = new JLabel("Nombre de la empresa:");
-        empresa.setForeground(Color.white);
-        Font fuenteEmpresa = new Font("Arial", Font.BOLD, 14);
-        empresa.setFont(fuenteEmpresa);
-        empresa.setBounds(0, 0, 300, 150);
-        Clientes.add(empresa);
-
-        JTextField empresatxt = new JTextField("");
-        empresatxt.setForeground(Color.black);
-        empresatxt.setBorder(null);
-        empresatxt.setBounds(170, 63, 200, 30);
-        Clientes.add(empresatxt);
-
-        JLabel nombreResponsable = new JLabel("Información cliente:");
-        nombreResponsable.setForeground(Color.black);
-        Font fuenteResponsable = new Font("Arial", Font.BOLD, 14);
-        nombreResponsable.setFont(fuenteResponsable);
-        nombreResponsable.setBounds(260, 0, 280, 230);
-        Clientes.add(nombreResponsable);
-
-        JLabel nombresResponsable = new JLabel("Nombres:");
-        nombresResponsable.setForeground(Color.white);
-        Font fuenteResponsables = new Font("Arial", Font.BOLD, 14);
-        nombresResponsable.setFont(fuenteResponsables);
-        nombresResponsable.setBounds(0, 30, 280, 230);
-        Clientes.add(nombresResponsable);
-
-        JTextField NombreResponsabletxt = new JTextField("");
-        NombreResponsabletxt.setForeground(Color.black);
-        NombreResponsabletxt.setBounds(128, 135, 120, 30);
-        NombreResponsabletxt.setBorder(null);
-        Clientes.add(NombreResponsabletxt);
-
-        JLabel ApellidoResponsablePaterno = new JLabel("Apellido paterno:");
-        ApellidoResponsablePaterno.setForeground(Color.white);
-        Font fuenteResponsablesP = new Font("Arial", Font.BOLD, 14);
-        ApellidoResponsablePaterno.setFont(fuenteResponsablesP);
-        ApellidoResponsablePaterno.setBounds(290, 30, 350, 230);
-        Clientes.add(ApellidoResponsablePaterno);
-
-        JTextField ApellidoResponsablePaternotxt = new JTextField("");
-        ApellidoResponsablePaternotxt.setForeground(Color.black);
-        ApellidoResponsablePaternotxt.setBounds(436, 130, 120, 30);
-        ApellidoResponsablePaternotxt.setBorder(null);
-        Clientes.add(ApellidoResponsablePaternotxt);
-
-        JLabel ApellidoResponsableMaterno = new JLabel("Apellido materno:");
-        ApellidoResponsableMaterno.setForeground(Color.white);
-        Font fuenteResponsablesMaterno = new Font("Arial", Font.BOLD, 14);
-        ApellidoResponsableMaterno.setFont(fuenteResponsables);
-        ApellidoResponsableMaterno.setBounds(0, 70, 280, 230);
-        Clientes.add(ApellidoResponsableMaterno);
-
-        JTextField ApellidoResponsableMaternotxt = new JTextField("");
-        ApellidoResponsableMaternotxt.setForeground(Color.black);
-        ApellidoResponsableMaternotxt.setBounds(128, 176, 120, 30);
-        ApellidoResponsableMaternotxt.setBorder(null);
-        Clientes.add(ApellidoResponsableMaternotxt);
-
-        JLabel Telefono = new JLabel("Télefono del cliente:");
-        Telefono.setForeground(Color.white);
-        Font fuenteTelefono = new Font("Arial", Font.BOLD, 14);
-        Telefono.setFont(fuenteTelefono);
-        Telefono.setBounds(290, 115, 300, 150);
-        Clientes.add(Telefono);
-
-        JTextField Telefonotxt = new JTextField("");
-        Telefonotxt.setForeground(Color.black);
-        Telefonotxt.setBounds(436, 175, 120, 30);
-        Telefonotxt.setBorder(null);
-        Clientes.add(Telefonotxt);
-
-        JLabel Correo = new JLabel("Correo electrónico del cliente:");
-        Correo.setForeground(Color.white);
-        Font fuenteCorreo = new Font("Arial", Font.BOLD, 14);
-        Correo.setFont(fuenteCorreo);
-        Correo.setBounds(0, 155, 280, 150);
-        Clientes.add(Correo);
-
-        JTextField Correotxt = new JTextField("");
-        Correotxt.setForeground(Color.black);
-        Correotxt.setBounds(220, 218, 210, 30);
-        Correotxt.setBorder(null);
-        Clientes.add(Correotxt);
-
-        JLabel Domicilio = new JLabel("Datos del domicilio del cliente:");
-        Domicilio.setForeground(Color.BLACK);
-        Font fuenteDomicilio = new Font("Arial", Font.BOLD, 14);
-        Domicilio.setFont(fuenteDomicilio);
-        Domicilio.setBounds(225, 137, 300, 300);
-        Clientes.add(Domicilio);
-
-        JLabel Calle = new JLabel("Calle:");
-        Calle.setForeground(Color.white);
-        Font fontCalle = new Font("Arial", Font.BOLD, 14);
-        Calle.setFont(fontCalle);
-        Calle.setBounds(0, 159, 300, 300);
-        Clientes.add(Calle);
-
-        JTextField Calletxt = new JTextField("");
-        Calletxt.setForeground(Color.black);
-        Calletxt.setBounds(45, 300, 170, 30);
-        Calletxt.setBorder(null);
-        Clientes.add(Calletxt);
-
-        JLabel Numero = new JLabel("Número:");
-        Numero.setForeground(Color.white);
-        Font fontNumero = new Font("Arial", Font.BOLD, 14);
-        Numero.setFont(fontNumero);
-        Numero.setBounds(250, 159, 300, 300);
-        Clientes.add(Numero);
-
-        JTextField Numtxt = new JTextField("");
-        Numtxt.setForeground(Color.black);
-        Numtxt.setBounds(325, 300, 40, 30);
-        Numtxt.setBorder(null);
-        Clientes.add(Numtxt);
-
-        JLabel colonia = new JLabel("Colonia:");
-        colonia.setForeground(Color.white);
-        Font fontColonia = new Font("Arial", Font.BOLD, 14);
-        colonia.setFont(fontColonia);
-        colonia.setBounds(390, 159, 300, 300);
-        Clientes.add(colonia);
-
-        JTextField Coltxt = new JTextField();
-        Coltxt.setForeground(Color.black);
-        Coltxt.setBounds(450, 300, 200, 30);
-        Coltxt.setBorder(null);
-        Clientes.add(Coltxt);
-
-        JLabel Municipio = new JLabel("Municipio:");
-        Municipio.setForeground(Color.white);
-        Font fontMunicipio = new Font("Arial", Font.BOLD, 14);
-        Municipio.setFont(fontMunicipio);
-        Municipio.setBounds(0, 210, 300, 300);
-        Clientes.add(Municipio);
-
-        //JComboBox Municipiotxt = new JComboBox();
-        JTextField Municipiotxt = new JTextField();
-        Municipiotxt.setForeground(Color.black);
-        Municipiotxt.setBounds(75, 350, 200, 30);
-        Municipiotxt.setBorder(null);
-        Clientes.add(Municipiotxt);
-
-        JLabel Estado = new JLabel("Estado:");
-        Estado.setForeground(Color.white);
-        Font fontEstado = new Font("Arial", Font.BOLD, 14);
-        Estado.setFont(fontEstado);
-        Estado.setBounds(300, 210, 100, 300);
-        Clientes.add(Estado);
-
-        JTextField Estadotxt = new JTextField();
-        Estadotxt.setForeground(Color.black);
-        Estadotxt.setBounds(355, 350, 170, 30);
-        Estadotxt.setBorder(null);
-        Clientes.add(Estadotxt);
-
-        JButton Agregar = new JButton("Agregar cliente");
-        Agregar.setBackground(Color.decode("#049cff"));
-        //entrar.setBounds(110, 350, 150, 50);
-        Agregar.setBorder(new ComponenteBotonRedondo(40));
-        Agregar.setForeground(Color.black);
-        Agregar.setBounds(250, 400, 200, 50);
-        Clientes.add(Agregar);
-
-        JLabel background = new JLabel();
-
-        background.add(Clientes);
-
-        add(background);
-
-        setVisible(true);
-
-    }
-
-    public static void main(String[] args) {
-
-        new AgregarCliente();
-    }
+	}
+	
+	public static void main(String[]args){
+		
+		new AgregarCliente();
+	}
+	
+	
 
 }
